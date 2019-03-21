84c6f7a074e23f7aaef679c5ee06badf8a671c7f
gitminuro <kotoribuki@gmail.com>
2019-03-21 16:45:26 +0900

    メイン画面のボタン作成

--- app/src/main/java/com/gitminuro/numbertouch/MainActivity.java
+++ app/src/main/java/com/gitminuro/numbertouch/MainActivity.java
@@ -4,8 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] numArray;
+    private int[] sortArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * タイトル画面の表示
      */
+
     private void setLayoutTitle() {
         setContentView(R.layout.activity_title);
 
@@ -56,12 +63,55 @@ public class MainActivity extends AppCompatActivity {
     private void setLayoutMain() {
         setContentView(R.layout.activity_main);
 
-        Button b = (Button)findViewById(R.id.button2);
+        View.OnClickListener event = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView txt = (TextView) findViewById(R.id.TxtTest);
+                txt.setText(((Button)v).getText());
+            }
+        };
+
+        setRandomNum();
+        setButton(event, R.id.button1, 1);
+        setButton(event, R.id.button2, 2);
+        setButton(event, R.id.button3, 3);
+        setButton(event, R.id.button4, 4);
+        setButton(event, R.id.button5, 5);
+        setButton(event, R.id.button6, 6);
+        setButton(event, R.id.button7, 7);
+        setButton(event, R.id.button8, 8);
+        setButton(event, R.id.button9, 9);
+        setButton(event, R.id.button10, 10);
+        setButton(event, R.id.button11, 11);
+        setButton(event, R.id.button12, 12);
+        setButton(event, R.id.button13, 13);
+        setButton(event, R.id.button14, 14);
+        setButton(event, R.id.button15, 15);
+        setButton(event, R.id.button16, 16);
+        setButton(event, R.id.button17, 17);
+        setButton(event, R.id.button18, 18);
+        setButton(event, R.id.button19, 19);
+        setButton(event, R.id.button20, 20);
+
+        Button b = (Button)findViewById(R.id.title);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setLayoutTitle();
             }
         });
+    }
+
+    private void setRandomNum() {
+        numArray = new int[20];
+        for(int i = 0; i < 20; i++) {
+            numArray[i] = new Random().nextInt(100) + 1;
+        }
+    }
+
+    private void setButton(View.OnClickListener ev, int btnId, int renban) {
+        Button numBtn = (Button)findViewById(btnId);
+        numBtn.setOnClickListener(ev);
+        numBtn.setText(new Integer(numArray[renban-1]).toString());
     }
 }
